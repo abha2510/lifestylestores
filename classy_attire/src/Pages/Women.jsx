f00ce164f1b09f0c811ce4c0ead9cffb8d870c33
Abha Meshram <abha25meshram@gmail.com>
2022-12-17 00:38:42 +0530

    add day-4

--- classy_attire/src/Pages/Women.jsx
+++ classy_attire/src/Pages/Women.jsx
@@ -1,7 +1,4 @@
-import React from 'react'
-// import  Womenbags  from '../Components/Subpages/Womenbags'
-// import  Womenbottom  from '../Components/Subpages/Womenbottom'
-// import  WomenTopWear  from '../Components/Subpages/WomentopWear'
+import React from 'react';
 import Carousel2 from './Carousel2';
 import "./Women.css";
 import {Link as RouterLink} from "react-router-dom"
